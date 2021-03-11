938331ed04099419e7267dc80386d56eb06e19d6
Afsarsoft <15915242+Afsarsoft@users.noreply.github.com>
2021-03-11 14:52:39 -0800

    ...

--- Test/Test101.sql
+++ Test/Test101.sql
@@ -14,6 +14,16 @@ SELECT *
 FROM game.Type;
 
 
+-- TRUNCATE TABLE game.Retailer;
+-- TRUNCATE TABLE game.Discount;
+-- TRUNCATE TABLE game.[Order];
+-- TRUNCATE TABLE game.Game;
+-- TRUNCATE TABLE game.Type;
+-- TRUNCATE TABLE game.GameTeam;
+-- TRUNCATE TABLE game.Team;
+-- TRUNCATE TABLE game.Partner;
+-- TRUNCATE TABLE game.PartnerInfo;
+
 
 
 
