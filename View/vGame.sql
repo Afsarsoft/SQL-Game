db212b46340145c0289cd5a5416dcb76d901c888
Afsarsoft <15915242+Afsarsoft@users.noreply.github.com>
2021-03-11 07:49:47 -0800

    ...

--- View/vGame.sql
+++ View/vGame.sql
@@ -2,7 +2,7 @@ CREATE OR ALTER VIEW game.vGame
 AS
     SELECT G.GameID, G.Name, T.Name AS GameType, P.Name AS Partner, G.Note
     FROM game.Game G
-        INNER JOIN game.vPartner P
+        INNER JOIN game.Partner P
         ON G.PartnerID = P.PartnerID
         INNER JOIN game.Type T
         ON G.TypeID = T.TypeID
